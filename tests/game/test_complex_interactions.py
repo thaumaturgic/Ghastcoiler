c6e4281c43eb8de8f0da5d8d8a403d76e775a3f6
thaumaturgic <scott.brust@gmail.com>
2021-06-02 21:38:54 -0700

    Reborn test cases with minion addition/removal, still TODO

--- tests/game/test_complex_interactions.py
+++ tests/game/test_complex_interactions.py
@@ -1,6 +1,7 @@
 from ghastcoiler.minions.test_minions import PunchingBag
-from ghastcoiler.minions.rank_1 import AcolyteOfCThun, Scallywag
-from ghastcoiler.minions.rank_2 import HarvestGolem, KaboomBot
+from ghastcoiler.minions.rank_1 import AcolyteOfCThun, MicroMummy, Scallywag
+from ghastcoiler.minions.rank_2 import HarvestGolem, Imprisoner, KaboomBot
+from minions.rank_3 import SoulJuggler
 
 
 def test_baron_rivendare(initialized_game):
@@ -37,17 +38,7 @@ def test_reborn_mechanic(initialized_game):
     attacker_board = initialized_game.player_board[0]
     defender_board = initialized_game.player_board[1]
 
-    # Deathrattle + reborn order version 1: adding minions
-    attacker_board.add_minion(HarvestGolem(reborn=True))
-    defender_board.add_minion(PunchingBag(attack=10))
-    initialized_game.start_of_game()
-    initialized_game.single_round()
-    # TODO: Fix
-    # assert attacker_board.minions[0].name == "Damaged Golem"
-    # assert attacker_board.minions[1].name == "Harvest Golem"
-    assert True
-
-    # Deathrattle + reborn order version 2: killing minions
+    # Deathrattle + reborn order
     reborn_bomb = KaboomBot(reborn=True)
     attacker_board.set_minions([reborn_bomb])
     defender_board.set_minions([KaboomBot()])
@@ -74,6 +65,32 @@ def test_reborn_mechanic(initialized_game):
     initialized_game.single_round()
     assert attacker_board.minions[6].name == "Damaged Golem"
 
+
+def test_reborn_position_tracking(initialized_game):
+    attacker_board = initialized_game.player_board[0]
+    defender_board = initialized_game.player_board[1]
+
+    # Deathrattle + reborn position when adding minions
+    attacker_board.add_minion(HarvestGolem(reborn=True))
+    defender_board.add_minion(PunchingBag(attack=10))
+    initialized_game.start_of_game()
+    initialized_game.single_round()
+    # TODO: Fix
+    #assert attacker_board.minions[0].name == "Damaged Golem"
+    #assert attacker_board.minions[1].name == "Harvest Golem"
+    assert True
+
+    # Death trigger + reborn position when killing minions
+    # Mummy attacks imp, imp and mummy die, 
+    # soul juggler kills punching bag, mummy should be reborn at position 0
+    attacker_board.set_minions([PunchingBag(health=1, attack=0), MicroMummy()])
+    defender_board.set_minions([Imprisoner(health=1), SoulJuggler()])
+
+    initialized_game.start_of_game(starting_player=0)
+    initialized_game.single_round()
+    # TODO: Fix
+    #assert attacker_board.minions[0].position == 0
+    assert True
 
 def test_golden_tokens(initialized_game):
     attacker_board = initialized_game.player_board[0]
